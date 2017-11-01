a3717be560a7f32a16b76e749e3339fac128d6a3
nirparai <nirparai@yahoo.com>
2017-11-01 20:55:21 +0545

    settings.py modified

--- hamronepal/settings.py
+++ hamronepal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yyh-2eoju@+vm9%v_bp2rfoy8kmgnf-g#^kr)rhyj!j41+)p^!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
