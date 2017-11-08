bab7507464c9495c7bb80262faf4bb82e5a7a696
binabade <bina.bade.c1@gmail.com>
2017-11-08 11:32:15 +0545

    urls.py modified

--- hamronepal/urls.py
+++ hamronepal/urls.py
@@ -14,3 +14,10 @@ urlpatterns = [
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
