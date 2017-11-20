a38cc50bd2fc9ce3aac0ee19a561ad5236f27171
Nirpa Hang Rai <nirparai@yahoo.com>
2017-11-20 20:32:26 +0545

    Update sample_wsgi.py

--- sample_wsgi.py
+++ sample_wsgi.py
@@ -6,7 +6,7 @@ path = '/home/nirpa/hamronepal'
 if path not in sys.path:
 	sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODUEL'] = 'hamronepal.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hamronepal.settings'
 
 from django.core.wsgi import get_wsgi_application
 from django.contrib.staticfiles.handlers import StaticFilesHandler
